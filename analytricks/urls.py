12c6a20b2f293724507cdaccef80c6ecc3fb7cc3
asadbukharee <asadbukharee@gmail.com>
2021-06-07 11:41:02 -0700

    Analytricks going online

---
+++ analytricks/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path,include
+import mainpage
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('mainpage.urls'),name='main')
+]
